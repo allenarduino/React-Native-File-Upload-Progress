86d94d6ca2d6c62db0b8629ced5f8fefc9eebd96
ALLEN <aljay3334@gmail.com>
2022-03-17 19:56:58 +0000

    Added the config folder containing the file for constants

---
+++ src/config/Constants.js
@@ -0,0 +1,9 @@
+export const Constants = {
+  borderRadius: 3,
+  borderWidth: 1,
+  borderColor: 'rgb(150, 150, 236)',
+  buttonTextColor: 'rgb(79, 79, 228)',
+  fontSize: 15,
+  fontFamily: 'ans-serif-condensed',
+  baseURL: 'https://ipfs-dev.ternoa.dev',
+};
